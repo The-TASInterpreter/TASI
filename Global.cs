7f34fb1fb47043a3626429ffcb59f4386d85ceba
Ekischleki <e.wolfschlaeger@gmail.com>
2023-03-22 17:54:33 +0100

    Fix methods being added twice to the namespaces

--- Global.cs
+++ Global.cs
@@ -17,42 +17,43 @@
             Namespaces[0].namespaceMethods.Add(new Method("DefFunc", VarDef.evarType.Void, Namespaces[0], new List<VarDef> { new VarDef(VarDef.evarType.String, "MethodName"), new VarDef(VarDef.evarType.String, "ReturnType"), new VarDef(VarDef.evarType.String, "ArgumentArray", true) })); //INF.DefFunc string, string, string array
             Namespaces[0].namespaceMethods.Add(new Method("Invoke", VarDef.evarType.Void, Namespaces[0], new List<VarDef> { new VarDef(VarDef.evarType.String, "PermitLine0") })); //INF.Invoke String(Permit)
             */
+            Namespaces = new();
             Namespaces.Add(new NamespaceInfo(NamespaceInfo.NamespaceIntend.Internal, "Test"));
-            Namespaces[0].namespaceMethods.Add(new("HelloWorld", VarDef.evarType.Void, Namespaces[0], new List<List<VarDef>> {
+            new Method("HelloWorld", VarDef.evarType.Void, Namespaces[0], new List<List<VarDef>> {
                 new List<VarDef> { new(VarDef.evarType.Bool, "display"), new(VarDef.evarType.String, "text")}
-            }));
+            });
 
             Namespaces.Add(new NamespaceInfo(NamespaceInfo.NamespaceIntend.Internal, "Console"));
-            Namespaces[1].namespaceMethods.Add(new("WriteLine", VarDef.evarType.Void, Namespaces[1], new List<List<VarDef>> {
+            new Method("WriteLine", VarDef.evarType.Void, Namespaces[1], new List<List<VarDef>> {
                 new List<VarDef> { new(VarDef.evarType.String, "text")},
                 new List<VarDef> { new(VarDef.evarType.Num, "num")},
                 new List<VarDef> { new(VarDef.evarType.Bool, "bool")}
-            }));
-            Namespaces[1].namespaceMethods.Add(new("Write", VarDef.evarType.Void, Namespaces[1], new List<List<VarDef>> {
+            });
+            new Method("Write", VarDef.evarType.Void, Namespaces[1], new List<List<VarDef>> {
                 new List<VarDef> { new(VarDef.evarType.String, "text")}
-            }));
-            Namespaces[1].namespaceMethods.Add(new("ReadLine", VarDef.evarType.String, Namespaces[1], new List<List<VarDef>> {
+            });
+            new Method("ReadLine", VarDef.evarType.String, Namespaces[1], new List<List<VarDef>> {
                 new List<VarDef> { new(VarDef.evarType.Bool, "showTextWhenTyping")},
                 new List<VarDef> {}
-            }));
-            Namespaces[1].namespaceMethods.Add(new("Clear", VarDef.evarType.Void, Namespaces[1], new List<List<VarDef>> {
+            });
+            new Method("Clear", VarDef.evarType.Void, Namespaces[1], new List<List<VarDef>> {
                 new List<VarDef> {}
-            }));
+            });
 
             Namespaces.Add(new NamespaceInfo(NamespaceInfo.NamespaceIntend.Internal, "Programm"));
-            Namespaces[2].namespaceMethods.Add(new("Pause", VarDef.evarType.Void, Namespaces[2], new List<List<VarDef>> {
+            new Method("Pause", VarDef.evarType.Void, Namespaces[2], new List<List<VarDef>> {
                 new List<VarDef> {},
                 new List<VarDef> {new(VarDef.evarType.Bool, "showPausedMessage")}
-            }));
+            });
 
             Namespaces.Add(new NamespaceInfo(NamespaceInfo.NamespaceIntend.Internal, "Inf"));
-            Namespaces[3].namespaceMethods.Add(new("DefVar", VarDef.evarType.Void, Namespaces[3], new List<List<VarDef>> {
+            new Method("DefVar", VarDef.evarType.Void, Namespaces[3], new List<List<VarDef>> {
                 new List<VarDef> {new(VarDef.evarType.String, "VarType"), new(VarDef.evarType.String, "VarName")}
-            }));
+            });
             Namespaces.Add(new NamespaceInfo(NamespaceInfo.NamespaceIntend.Internal, "Convert"));
-            Namespaces[4].namespaceMethods.Add(new("ToNum", VarDef.evarType.Num, Namespaces[4], new List<List<VarDef>> {
+            new Method("ToNum", VarDef.evarType.Num, Namespaces[4], new List<List<VarDef>> {
                 new List<VarDef> {new(VarDef.evarType.String, "ConvertFrom"), new(VarDef.evarType.Bool, "errorOnParseFail")}
-            }));
+            });
 
         }
     }

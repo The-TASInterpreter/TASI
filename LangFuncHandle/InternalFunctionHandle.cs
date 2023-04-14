a4fe826fea28ee00cf9e4c6a6728780402bfd5e4
Ekischleki <e.wolfschlaeger@gmail.com>
2023-04-14 21:51:11 +0200

    Fixed all type not working

--- LangFuncHandle/InternalFunctionHandle.cs
+++ LangFuncHandle/InternalFunctionHandle.cs
@@ -32,8 +32,12 @@
                     return null;
                 case "inf.defvar":
 
-                    if (!Enum.TryParse<Value.ValueType>(input[0].StringValue, true, out Value.ValueType varType)) throw new CodeSyntaxException($"The vartype \"{input[0].StringValue}\" doesn't exist.");
-
+                    if (!Enum.TryParse<Value.ValueType>(input[0].StringValue, true, out Value.ValueType varType) && input[0].StringValue != "all") throw new CodeSyntaxException($"The vartype \"{input[0].StringValue}\" doesn't exist.");
+                    if (input[0].StringValue == "all")
+                    {
+                        accessableObjects.accessableVars.Add(new(new(VarConstruct.VarType.all, input[1].StringValue), new(varType)));
+                        return null;
+                    }
                     accessableObjects.accessableVars.Add(new(new(Value.ConvertValueTypeToVarType(varType), input[1].StringValue), new(varType)));
                     return null;
                 case "convert.tonum":

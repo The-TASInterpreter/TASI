7746d3df31deafb49cd5d64ea7119a89e9d65980
Ekischleki <e.wolfschlaeger@gmail.com>
2023-04-24 21:11:29 +0200

    Implemented logic for the tree. Does it work? Idk imma find out very soon.

--- Objects/Tree/TreeElement.cs
+++ Objects/Tree/TreeElement.cs
@@ -2,6 +2,74 @@
 {
     public class TreeElement
     {
+
+        public Value? SimulateTree(Value? provided, AccessableObjects accessableObjects, out bool foundMatch)
+        {
+            switch (elementType)
+            {
+                case ElementType.Compare:
+                    Value valueOfCheck = Statement.GetValueOfCommandLine(checkLine, accessableObjects);
+                    if (provided == null) throw new CodeSyntaxException("You need to provide a Value to use a Compare branch.");
+                    if (provided.valueType == valueOfCheck.valueType && provided.ObjectValue.Equals(valueOfCheck.ObjectValue))
+                    {
+                        foundMatch = true;
+                        if (isBranch)
+                        {
+                            bool foundInternalMatchTracker = false;
+                            bool foundInternalMatch = false;
+                            foreach (TreeElement treeElement in subBranch)
+                            {
+                                Value returnedValue = null;
+                                if (treeElement.elementType == ElementType.Break && foundInternalMatchTracker)
+                                    return null;
+                                if (!(treeElement.elementType == ElementType.Else && foundInternalMatchTracker))
+                                    returnedValue = treeElement.SimulateTree(Statement.GetValueOfCommandLine(provide, accessableObjects), accessableObjects, out foundInternalMatch);
+                                if (foundInternalMatch) foundInternalMatchTracker = true;
+                                if (returnedValue != null) return returnedValue;
+                            }
+                        }
+                        else
+                        {
+                            return InterpretMain.InterpretNormalMode(doCode.commands, accessableObjects);
+                        }
+                    }
+                    foundMatch = false;
+                    return null;
+                case ElementType.Always or ElementType.Else:
+                    foundMatch = false;
+                    return InterpretMain.InterpretNormalMode(doCode.commands, accessableObjects);
+                case ElementType.Check:
+                    valueOfCheck = Statement.GetValueOfCommandLine(checkLine, accessableObjects);
+                    if (valueOfCheck.GetBoolValue)
+                    {
+                        foundMatch = true;
+                        if (isBranch)
+                        {
+                            bool foundInternalMatchTracker = false;
+                            bool foundInternalMatch = false;
+                            foreach (TreeElement treeElement in subBranch)
+                            {
+                                Value returnedValue = null;
+                                if (treeElement.elementType == ElementType.Break && foundInternalMatchTracker)
+                                    return null;
+                                if (!(treeElement.elementType == ElementType.Else && foundInternalMatchTracker))
+                                    returnedValue = treeElement.SimulateTree(Statement.GetValueOfCommandLine(provide, accessableObjects), accessableObjects, out foundInternalMatch);
+                                if (foundInternalMatch) foundInternalMatchTracker = true;
+                                if (returnedValue != null) return returnedValue;
+                            }
+                        }
+                        else
+                        {
+                            return InterpretMain.InterpretNormalMode(doCode.commands, accessableObjects);
+                        }
+                    }
+                    foundMatch = false;
+                    return null;
+                default:
+                    throw new InternalInterpreterException($"Tried to execute a {elementType}-type");
+            }
+        }
+
         public enum ElementType
         {
             Compare,    // |> "SthElse" => [Something];

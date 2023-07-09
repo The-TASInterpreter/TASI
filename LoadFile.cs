55f9f949087327347e484479aea27ca34de4887f
Ekischleki <e.wolfschlaeger@gmail.com>
2023-07-09 15:38:25 +0200

    Using String Builders in file loading is now about 1000% faster on large scales

--- LoadFile.cs
+++ LoadFile.cs
@@ -1,4 +1,5 @@
-﻿using static TASI.Command;
+﻿using System.Text;
+using static TASI.Command;
 
 namespace TASI
 {
@@ -6,30 +7,30 @@ namespace TASI
     {
         public static List<Command> ByPath(string location, bool autoAddToGlobal = true)
         {
+            StringBuilder sb = new();
             location = location.Trim('"');
             if (!File.Exists(location)) throw new CodeSyntaxException("The entered file doesn't exist.");
             List<string> codeFile = File.ReadAllLines(location).ToList();
             for (int i = 0; i < codeFile.Count; i++)
             {
-                string lineWithoutCommands = "";
+                sb.Clear();
                 for (int j = 0; j < codeFile[i].Length; j++)
                 {
-                    if ((codeFile[i][j] == '#' && j == 0) || (codeFile[i][j] == '#' && codeFile[i][j - 1] != '\\')) break; //Remove what comes next in the line, if there is a comment
-                    lineWithoutCommands += codeFile[i][j];
+                    if (codeFile[i][j] == '#') break; //Remove what comes next in the line, if there is a comment
+                    if (codeFile[i][j] == 'Ⅼ') throw new CodeSyntaxException($"Uhhhhmmm this is a weird error now. So basically, on line {i + 1} you used a character, that is already used by TASI to map code to lines (The character is:(I would have inserted it here right now, but the console can't even print this char. It looks like an L, but it's a bit larger.)). I picked this character, because I thought noone would use it directly in their code. Well, seems like I thought wrong... Simply said, you must remove this character from your code. But fear now! With the return statement \"lineChar\", you can paste this char into strings and stuff. I hope this character is worth the errors with lines! I'm sorry.\n-Ekischleki");
+                    sb.Append(codeFile[i][j]);
                 }
-                codeFile[i] = lineWithoutCommands;
+                codeFile[i] = sb.ToString();
             }
 
-            string allFileCode = "";
+            sb.Clear();
             for (int i = 0; i < codeFile.Count; i++)
             {
-                string line = codeFile[i];
-                if (line.Contains('Ⅼ')) throw new CodeSyntaxException($"Uhhhhmmm this is a weird error now. So basically, on line {i + 1} you used a character, that is already used by TASI to map code to lines (The character is:(I would have inserted it here right now, but the console can't even print this char. It looks like an L, but it's a bit larger.)). I picked this character, because I thought noone would use it directly in their code. Well, seems like I thought wrong... Simply said, you must remove this character from your code. But fear now! With the return statement \"lineChar\", you can paste this char into strings and stuff. I hope this character is worth the errors with lines! I'm sorry.\n-Ekischleki");
-                allFileCode += $"Ⅼ{i}Ⅼ{line}";
+                sb.Append($"Ⅼ{i}Ⅼ{codeFile[i]}");
             }
             if (autoAddToGlobal)
                 Global.allLoadedFiles.Add(location);
-            return StringProcess.ConvertLineToCommand(allFileCode);
+            return StringProcess.ConvertLineToCommand(sb.ToString());
 
         }
         public static Value? RunCode(string code)

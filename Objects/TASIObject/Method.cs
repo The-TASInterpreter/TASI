4938ac46b6b570952bd1cc35d5a575407bbea19f
Ekischleki <e.wolfschlaeger@gmail.com>
2023-07-23 19:00:14 +0200

    added more object infrastructure

---
+++ Objects/TASIObject/Method.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TASI.Objects.TASIObject
+{
+    public class Method
+    {
+        TASIObjectDefinition parentType;
+        string methodName;
+        NamespaceInfo parentNamespace;
+        public VarConstruct.VarType? returnType;
+        public List<List<VarConstruct>> methodArguments;
+        public List<CommandLine> methodCode;
+
+
+
+    }
+}

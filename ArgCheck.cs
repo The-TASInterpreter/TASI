f5b7d5fc2e5acb695599094afd674796e18f0ec1
Ekischleki <e.wolfschlaeger@gmail.com>
2023-08-11 11:42:49 +0200

    Implemented an argument interpreter

--- ArgCheck.cs
+++ ArgCheck.cs
@@ -1,17 +1,131 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TASI
+﻿namespace TASI
 {
     internal class ArgCheck
     {
-        public static void CheckArgs()
+        public static readonly List<ArgDefinition> argCommandsDefinitions = new()
         {
+            new("p", "enables you to import plugins. usage: /p <plugin path>", new() {1}, true ),
+            new("r", "specifys the file that will be interpreted. usage: /r <file path>", new() {1 }, false),
+            new("a", "loads a namespace specifyed and auto-imports it to every file used. usage /a <namespace path>", new() {1}, true)
+
+
+        };
+
+        public static void InterpretArguments(List<ArgInstance> tokens)
+        {
+            foreach (ArgInstance token in tokens)
+            {
+                switch (token.argDefinition.argName)
+                {
+                    case "p":
+                        throw new NotImplementedException();
+                    case "r":
+                        throw new NotImplementedException();
+                    case "a":
+                        throw new NotImplementedException();
+                    default:
+                        throw new InternalInterpreterException($"Not implemented argument {token.argDefinition.argName}");
+                }
+                token.AlreadyUsed = true;
+            }
+        }
+
+
+
+        public static List<ArgInstance> TokeniseArgs(string[] args, List<ArgDefinition> argDefinitions)
+        {
+            ArgInstance? currentArg = null;
+            List<ArgInstance> result = new();
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                string? arg = args[i];
+                if (arg[0] == '/' || arg[0] == '-')
+                {
+                    if (currentArg != null)
+                    {
+                        result.Add(currentArg);
+                    }
+
+                    if (arg.Length < 2)
+                    {
+                        throw new ArgumentException("There are no empty arguments (\"/\")");
+                    }
+                    string actualArg = arg[1..];
+                    currentArg = new(argDefinitions.FirstOrDefault(x => x.argName == actualArg) ?? throw new ArgumentException($"An argument with the name \"{actualArg}\" doesn't exist. Use the /help argument to see all arguments."));
+                    
+
+                } 
+                else
+                {
+                    if (currentArg == null)
+                        throw new ArgumentException("You must first define an argument before listing attributes.");
+                    currentArg.argAttributes.Add(arg);
+                }
+
+            }
+            if (currentArg != null)
+            {
+                result.Add(currentArg);
+            }
+            result.ForEach(a => a.VerifyAttributes());
+
+            return result;
+        }
+
+    }
+    internal class ArgInstance
+    {
+        public ArgDefinition argDefinition;
+        public List<string> argAttributes;
+        private bool alreadyUsed;
+        public bool AlreadyUsed
+        {
+            get
+            {
+                return alreadyUsed;
+            }
+            set
+            {
+                if (alreadyUsed == true)
+                    throw new ArgumentException($"The argument {argDefinition.argName} can only be used once");
+                alreadyUsed = value;
+                if (argDefinition.usableMultibleTimes)
+                    alreadyUsed = false;
+
+            }
+        }
+        public ArgInstance(ArgDefinition argDefinition)
+        {
+            this.argDefinition = argDefinition;
+            argAttributes = new();
 
         }
 
+        public void VerifyAttributes()
+        {
+            if (!argDefinition.argAttributes.Contains(argAttributes.Count))
+            {
+                throw new ArgumentException($"Invalid usage of {argDefinition.argName} argument. Type help to see correct uses");
+            }
+        }
+    }
+
+
+    internal class ArgDefinition
+    {
+        public string argName;
+        public string argDescription;
+        public List<int> argAttributes;
+        public readonly bool usableMultibleTimes;
+
+
+        public ArgDefinition(string argName, string argDescription, List<int> argAttributes, bool usableMultibleTimes)
+        {
+            this.usableMultibleTimes = usableMultibleTimes;
+            this.argName = argName;
+            this.argDescription = argDescription;
+            this.argAttributes = argAttributes;
+        }
     }
 }
